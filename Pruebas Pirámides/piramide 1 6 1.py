2994b7183d57d5541c7ab498f03600c625eb67d5
Izan <izan.arnaiz.7e5@itb.cat>
2023-12-12 21:26:08 +0100

    a

---
+++ Pruebas Pirámides/piramide 1 6 1.py
@@ -0,0 +1,18 @@
+
+
+comprobar = True
+while comprobar == True:
+    n = int(input(""))
+    if n > 0:
+        comprobar = False
+        for i in range (1, n + 1):
+            for x in range (1, i + 1):
+                print(x, end="")
+            print("")
+
+        for i in range (n-1, 0, -1):
+            for x in range (1, i + 1):
+                print(x, end="")
+            print("")
+    else:
+        print("El número debe ser positivo")
