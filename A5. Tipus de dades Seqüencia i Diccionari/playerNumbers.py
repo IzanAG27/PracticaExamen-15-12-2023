1bf1b384cf6e34e6768a77b5cd69702e79dbe7c4
Izan <izan.arnaiz.7e5@itb.cat>
2024-01-28 18:47:14 +0100

    SUM

--- A5. Tipus de dades Seqüencia i Diccionari/playerNumbers.py
+++ A5. Tipus de dades Seqüencia i Diccionari/playerNumbers.py
@@ -1,9 +1,6 @@
+JUGADORES = 5
+numeros = []
 
-j1 = input("\n")
-j2 = input("\n")
-j3 = input("\n")
-j4 = input("\n")
-j5 = input("\n")
-
-alineacio = [j1, j2, j3, j4, j5]
-
+for x in range(JUGADORES):
+    numeros.append(input(""))
+print(numeros)
