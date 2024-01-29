c09088e35195e5ebe02ac59bac1a2ad6d76fc841
izan.arnaiz.7e5 <izan.arnaiz.7e5@itb.cat>
2024-01-29 13:02:37 +0100

    SUM

--- A5. Tipus de dades Seqüencia i Diccionari/palindrom.py
+++ A5. Tipus de dades Seqüencia i Diccionari/palindrom.py
@@ -2,13 +2,18 @@ palabra = input("").split()
 devnull = []
 sinEspaciosON = ""
 sinEspaciosRV = ""
-
+ACENTOS = ('á', 'à', 'è', 'é', 'ì', 'í', 'ò', 'ó', 'ù', 'ú')
+VOWELS = ('a', 'a', 'e', 'e', 'i', 'i', 'o', 'o', 'u', 'u',)
 for x in palabra:
     if x == " ":
         devnull.append(devnull)
     else:
         sinEspaciosON += x.lower()
 
+for x in palabra:
+    if x in ACENTOS:
+        palabra.replace(x, VOWELS[ACENTOS.index(x)] )
+print(palabra)
 for x in sinEspaciosON[::-1]:
     sinEspaciosRV += x.lower()
 
