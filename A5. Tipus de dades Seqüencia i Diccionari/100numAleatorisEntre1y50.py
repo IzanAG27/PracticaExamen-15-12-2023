1d3cf4b191eaee80997639ac37bc17a26f24a707
izan.arnaiz.7e5 <izan.arnaiz.7e5@itb.cat>
2024-01-29 11:45:08 +0100

    SUM

---
+++ A5. Tipus de dades Seqüencia i Diccionari/100numAleatorisEntre1y50.py
@@ -0,0 +1,33 @@
+"""
+    Noms: Daniel Arco, Izan Arnáiz.
+    Grup: ASIXc1A
+    Data: 22/01/2024
+    Descripció:
+    Lista de 100 nombres aleatorios entre 1 y 50 y después hacer una media de los nombres pares
+    y después de los nombres impares.
+"""
+import random
+
+# Generem una llista de 100 numeros aleatoris entre el 1 y el 50 cadascun.
+nums = [random.randint(1,50) for x in range(100)]
+
+sumaParell = 0
+sumaSenar = 0
+nombreParell = 0
+nombreSenar = 0
+listaPS = []
+# Bucle del 0 al 99(100 Numeros)
+for i in range(0, 99):
+    if i % 2 == 0:
+        sumaParell += nums[i]
+        nombreParell += 1
+        listaPS.append(nums[i])
+    else:
+        sumaSenar += nums[i]
+        nombreSenar += 1
+        listaPS.append(nums[i])
+
+# Imprimim els parells i senars i els dividim per el nombre de Parells o Senars respectivament
+print(f"Amb els nombres: {listaPS}")
+print(f"mitjana posicions parelles: {sumaParell // nombreParell}.")
+print(f"mitjana posicions senars: {sumaSenar // nombreSenar}.")
