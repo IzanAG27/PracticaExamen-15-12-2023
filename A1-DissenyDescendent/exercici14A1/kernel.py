71664bb3746e492055b4bdc0209e7453a967c4bd
izan.arnaiz.7e5 <izan.arnaiz.7e5@itb.cat>
2024-04-03 09:42:11 +0200

    SUM

--- A1-DissenyDescendent/exercici14A1/kernel.py
+++ A1-DissenyDescendent/exercici14A1/kernel.py
@@ -1,19 +1,13 @@
 """
 Funcions
 """
-
-
+pila = []
 def menu():
     opcion = int(input("Menu:  \n  1. Afegir element a la pila\n  2. Treure element de la pila"
                        "\n  3. Longuitud de la pila\n  4. Mostrar pila\n  5. Sortir"))
     return opcion
-
-def pila():
-    pila = []
-    return pila
-
 def afegeixAPila():
-
+    pila = []
 def treureDeLaPila():
 
 def llargadaPila():
@@ -21,13 +15,3 @@ def llargadaPila():
 def mostrarPila():
 
 def estaBuidaPila():
-
-
-
-
-
-
-
-
-def mostrarPila():
-
