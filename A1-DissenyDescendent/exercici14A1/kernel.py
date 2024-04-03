fef0606eacaeb7f8607b7b3e928d8bb2719ca395
izan.arnaiz.7e5 <izan.arnaiz.7e5@itb.cat>
2024-04-03 09:33:41 +0200

    SUM

--- A1-DissenyDescendent/exercici14A1/kernel.py
+++ A1-DissenyDescendent/exercici14A1/kernel.py
@@ -1,3 +1,22 @@
 """
 Funcions
 """
+
+def menu():
+    opcion = int(input("1. Afegir element a la pila\n2. Treure element de la pila"
+            "\n3. Longuitud de la pila\n4. Mostrar pila\n5. Sortir""))
+    return opcion
+def llargadaPila():
+
+
+def estaBuidaPila():
+
+
+def afegeixAPila():
+
+
+def treureDeLaPila():
+
+
+def mostrarPila():
+
--- A1-DissenyDescendent/exercici14A1/main.py
+++ A1-DissenyDescendent/exercici14A1/main.py
@@ -1,3 +1,20 @@
 """
 Execució programa principal
 """
+import kernel
+
+comprobar = True
+while comprobar:
+    if kernel.menu() == 1:
+        kernel.afegeixAPila()
+    elif kernel.menu() == 2:
+        kernel.treureDeLaPila()
+    elif kernel.menu() == 3:
+        kernel.llargadaPila()
+    elif kernel.menu() == 4:
+        kernel.mostrarPila()
+    elif kernel.menu() == 5:
+        comprobar = False
+    else:
+        print("Número inválido!")
+        comprobar = False
