78f6bd4af3e3d89de7e51c6460b367ac5a7b0d7d
Izan <izan.arnaiz.7e5@itb.cat>
2024-04-04 17:25:54 +0200

    asd

---
+++ A2-DissenyModular/exerciciNoAvaluable1/caLL.py
@@ -0,0 +1,33 @@
+import string
+import re
+
+
+def contar_letras_y_caracteres(frase):
+    num_caracteres = len(frase.replace(" ", ""))
+    num_letras = len(re.findall(r'[a-zA-Z]', frase))
+    return num_caracteres, num_letras
+
+
+def validar_frase(frase):
+    palabras = frase.split()
+    return len(palabras)
+
+
+def calcularLletres():
+    frases = []
+    seguir = True
+    while seguir:
+        frase = input("")
+        if frase == "\\q":
+            seguir = False
+        elif not validar_frase(frase):
+            print("La frase debe contener más de dos palabras.")
+        else:
+            frases.append(frase)
+
+    for frase in frases:
+        num_caracteres, num_letras = contar_letras_y_caracteres(frase)
+        print(f"De {num_caracteres} caracteres {num_letras} son letras")
+
+
+calcularLletres()
