14fe90c01b3716e493a007123b1f64275e3e8304
izan.arnaiz.7e5 <izan.arnaiz.7e5@itb.cat>
2024-05-10 09:44:12 +0200

    examen10-05

--- PROJECTO-M03-CON-DANI/pp1-arnaizIzan-ASIXc1A/main.py
+++ PROJECTO-M03-CON-DANI/pp1-arnaizIzan-ASIXc1A/main.py
@@ -10,7 +10,20 @@
 
 # region imports
 from kernel import *
+
+
 # endregion
 
 # region main
+def main():
+    if not os.path.exists(INPUT_DIR):
+        logging.error("El directorio de entrada %s no se encuentra", INPUT_DIR)
+        return
+    if not os.path.exists(OUTPUT_DIR):
+        logging.error("El directorio de salida %s no se encuentra", INPUT_DIR)
+        return
+
+
+if __name__ == "__main__":
+    main()
 # endregion
